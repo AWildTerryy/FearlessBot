8c37da4584bd6cc7cba74b12c568b40a213e8b4e
AWildTerryy <daniel.tarsoo@gmail.com>
2022-11-24 04:04:17 -0500

    has the discord bot functionalities, just respond to messages

---
+++ bot.py
@@ -0,0 +1,42 @@
+import discord
+import responses
+
+
+async def send_message(message, user_message, is_private):
+    try:
+        response = responses.handle_response(user_message)
+        await message.author.send(response) if is_private else await message.channel.send(response)
+    except Exception as e:
+        print(e)
+
+def run_discord_bot():
+    token = None
+    intents = discord.Intents.default()
+    intents.message_content = True
+    client = discord.Client(intents=intents)
+
+    @client.event
+    async def on_ready():
+        print(f'{client.user} is now running!')
+
+    @client.event
+    async def on_message(message):
+        if message.author == client.user:
+            return
+        username = message.author
+        user_message = message.content
+        channel = message.channel
+
+        print(f'{username} said: "{user_message}" ({channel})')
+
+        if user_message[0] == '?':
+            user_message = user_message[1:]
+            await send_message(message, user_message, is_private=True)
+        elif user_message[0] == '!':
+            user_message = user_message[1:]
+            await send_message(message, user_message, is_private=False)
+        elif user_message == 'who':
+            await message.channel.send('asked') # just to test understanding
+        else:
+            return
+    client.run(token)
