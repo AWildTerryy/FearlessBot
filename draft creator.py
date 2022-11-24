4fe76af3400376f832f5dba70e401050e76d5264
AWildTerryy <daniel.tarsoo@gmail.com>
2022-11-24 00:15:35 -0500

    small change for selenium

--- draft creator.py
+++ draft creator.py
@@ -1,6 +1,5 @@
 # this script opens draftlol, ban champions from a list, then returns the draft links
 
-# from webdriver_manager.chrome import ChromeDriverManager
 from selenium import webdriver
 import time
 from os import path
@@ -8,22 +7,22 @@ from os import path
 browser = webdriver.Chrome()
 browser.get('https://draftlol.dawe.gg/')
 time.sleep(0.5)  # just to let page load, shouldn't take too long
-browser.find_element_by_class_name('advancedOptionsButton').click()
+browser.find_element('class name', 'advancedOptionsButton').click()
 time.sleep(0.5)  # let advance options load
-championSearchBox = browser.find_element_by_class_name('champsFiltersSearch')
-championSearch = championSearchBox.find_element_by_tag_name('input')  # get search box
+championSearchBox = browser.find_element('class name', 'champsFiltersSearch')
+championSearch = championSearchBox.find_element('tag name', 'input')  # get search box
 
 
 def ban_champion(champName):
     # This function will search for a champion, click it in order to ban it, then clear the search field
     championSearch.send_keys(champName)
-    champion = browser.find_element_by_class_name('roomImgListItem')
+    champion = browser.find_element('class name', 'roomImgListItem')
     browser.execute_script('arguments[0].click()', champion)
     championSearch.clear()
 
 
 if path.exists('bannedchampions.txt'):
-    banList = open('bannedchampions.txt','r')
+    banList = open('bannedchampions.txt', 'r')
     banArray = []
     for line in banList:
         if line != '\n':
@@ -34,10 +33,10 @@ if path.exists('bannedchampions.txt'):
 
 # if bannedchampions.txt exists, then an array is formed using the list and then banned
 # if the txt file is not there, a fresh draft will be made
-browser.find_element_by_class_name('sendButton').click()
+browser.find_element('class name', 'sendButton').click()
 time.sleep(0.5)  # After banning the champions, click the create button to create the draft and wait for it to load
-blueDraft = browser.find_element_by_class_name('inputBlue').get_property('defaultValue')
-redDraft = browser.find_element_by_class_name('inputRed').get_property('defaultValue')
+blueDraft = browser.find_element('class name', 'inputBlue').get_property('defaultValue')
+redDraft = browser.find_element('class name', 'inputRed').get_property('defaultValue')
 slicePoint = blueDraft.rindex('/')
 spectate = blueDraft[:slicePoint]
 # Get the links for the drafts, note that the spectate link is red or blue pick link before the last forward slash
