15cd648110d4a30f94482dd2fb5da7f792c681e7
AWildTerryy <daniel.tarsoo@gmail.com>
2022-11-28 23:19:35 -0500

    small comment

--- bot.py
+++ bot.py
@@ -8,7 +8,6 @@ async def send_message(message, user_message, is_private):
         await message.author.send(response) if is_private else await message.channel.send(response)
     except Exception as e:
         print(e)
-
 def run_discord_bot():
     token = None
     intents = discord.Intents.default()
@@ -36,7 +35,7 @@ def run_discord_bot():
             user_message = user_message[1:]
             await send_message(message, user_message, is_private=False)
         elif user_message == 'who':
-            await message.channel.send('asked') # just to test understanding
+            await message.channel.send('asked')  # just to test understanding
         else:
             return
     client.run(token)
